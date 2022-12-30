6bfbf38eb09704f26772c16b5348c1051e9030d4
Natanniel <natanniel.alves@outlook.com>
2022-12-30 09:44:25 -0300

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const server = require("./app");
 
-server.listen(3000, async function () {
+server.listen(3001, async function () {
   let data = new Date() 
   console.log("API Monstrobet em funcionamento ! " + data.toLocaleString() );
 });
